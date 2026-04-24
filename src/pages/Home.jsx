795176a71954e669339a5f064e637e6c3e9692da
Sebastian Garzón S <sebasgarzons88@gmail.com>
2026-04-23 19:22:23 -0500

    Implemented feedback on desestructure for try and catch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,16 @@ function Home() {
   const [gifs, setGifs] = useState([])
   const [search, setSearch] = useState('');
   const [hasSearched, setHasSearched] = useState(false);
+  const [error, setError] = useState(null)
 
   async function handleSearch() {
-    const results = await searchGifs(search)
-    setGifs(results)
-    setHasSearched(true);
+    const { data, error } = await searchGifs(search)
+    if (error){
+      setError(error) 
+    }else{
+      setGifs(data)
+      setHasSearched(true);
+    }
   }
 
 useEffect(() => {
@@ -24,20 +29,24 @@ useEffect(() => {
     return () => clearTimeout(debounce);
   }, [search]);
 
-
-  return (
-    <main>
-        <h1 className={!hasSearched ? 'visible title_home' : 'hidden-title title_home'}>Giphy Explorer</h1>
-
-        <section className='w-[88%] m-auto'>
-            <SearchBar onSearch={setSearch} />     
-            <GifContainer gifs={gifs} hasSearched={hasSearched} />
-        </section>
-
-
-      
-    </main>
-  )
+  if(error){
+    return (
+      <main>
+        {error && <p>{error}</p>}
+      </main>
+    )
+  }else{
+    return (
+      <main>
+          <h1 className={!hasSearched ? 'visible title_home' : 'hidden-title title_home'}>Giphy Explorer</h1>
+  
+          <section className='w-[88%] m-auto'>
+              <SearchBar onSearch={setSearch} />     
+              <GifContainer gifs={gifs} hasSearched={hasSearched} />
+          </section>       
+      </main>
+    )
+  }
 }
 
 export default Home
--- src/services/giphyApi.js
+++ src/services/giphyApi.js
@@ -2,7 +2,17 @@ const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
 const BASE_URL = 'https://api.giphy.com/v1/gifs'
 
 export async function searchGifs(query) {
-  const response = await fetch(`${BASE_URL}/search?api_key=${API_KEY}&q=${query}&limit=20`)
-  const data = await response.json()
-  return data.data
+  try {
+    const response = await fetch(`${BASE_URL}/search?api_key=${API_KEY}&q=${query}&limit=20`)
+    const data = await response.json()
+    return { 
+      data: data.data, 
+      error: null
+    }
+  } catch (err) {
+    return { 
+      data: null, 
+      error: 'Something went wrong' 
+    }
+  }
 }
